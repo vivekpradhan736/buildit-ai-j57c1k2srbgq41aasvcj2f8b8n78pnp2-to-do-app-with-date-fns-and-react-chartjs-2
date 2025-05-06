08feaaf7c7174200743b8d6b3aba3c25e4443a1a
Vivek Pradhan <vivekpradhan736@gmail.com>
2025-05-07 04:20:44 +0530

    Add src/components/ChartComponent.jsx from Buildit.ai

---
+++ src/components/ChartComponent.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Pie } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+function ChartComponent({ chartData }) {
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false, // Allow the chart to scale freely
+  };
+
+  return (
+    <div style={{ height: '300px', width: '100%' }}>
+      <Pie data={chartData} options={options} />
+    </div>
+  );
+}
+
+export default ChartComponent;
